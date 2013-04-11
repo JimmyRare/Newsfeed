7f3f808bb5dcf37d99d9b3235eeb7b7fc074907f
Jimmy Engstrom <jimmyrare@gmail.com>
2013-04-11 16:58:48 +0200

    Add menu and css

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,8 @@ var app = app || {};
 
 $(document).ready(function() {
 
+	new app.NewspaperView({ paper: 'ntAllt' });
+
 	$('.menu').on('click', '.menuLink', function(e) {
 		var rssFeed = $(this).attr('id');
 		e.preventDefault();
