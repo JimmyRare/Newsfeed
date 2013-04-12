4355912202827cf616d37a30d4a0f99c90fda6ba
Jimmy Engstrom <jimmyrare@gmail.com>
2013-04-12 12:34:09 +0200

    Add loader

--- public/js/views/newspaper.js
+++ public/js/views/newspaper.js
@@ -4,17 +4,31 @@ app.NewspaperView = Backbone.View.extend({
 	el: '.container',
 
 	initialize: function() {
+		var _this = this;
 		this.collection = new app.Newspaper({ paper: this.options.paper });
 		this.collection.shift(); // don't as me why it includes the paper object in the collection.. :S
-		this.collection.fetch({reset: true});
-		this.render();
+
+		// Init isotope
+		this.$el.isotope();
+
+		// Show loader
+		this.$el.empty();
+		$('#followingBallsG').show();
+
+		// Fetch rss
+		this.collection.fetch({reset: true}).complete(function() {
+			_this.render();
+		});
+
+		// Re-render on reset(fetch)
 		this.collection.on('reset', this.render, this);
 	},
 
 	render: function() {
 		var _this = this;
-
-		this.$el.empty();
+		//Hide loader
+		$('#followingBallsG').hide();
+		console.log('should be empty');
 		this.collection.each(function(article) {
 			this.renderArticle(article);
 		}, this);
