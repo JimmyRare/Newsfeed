9ac2744783717426d7118b81348d9a4946df4369
Jimmy Engstrom <jimmyrare@gmail.com>
2013-04-16 11:04:05 +0200

    Fix date object

--- public/js/views/article.js
+++ public/js/views/article.js
@@ -10,13 +10,29 @@ app.ArticleView = Backbone.View.extend({
 
 	initialize: function() {
 		// Format pubDate into time and date
-		var pubdate = this.model.attributes.pubdate,
-				date = pubdate.substr(0, 10),
-				time = pubdate.substr(11, 8),
-				dateAndTime = date + ' ' + time;
+		var months = ['Jan', 'Feb', 'Mar', 'Apr', 'Maj', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dec'],
+			days = ['Måndag', 'Tisdag', 'Onsdag', 'Torsdag', 'Fredag'],
+		  	pubdate = this.model.attributes.pubdate,
+			dateObj = new Date(pubdate),
+			year = dateObj.getFullYear(),
+			date = dateObj.getDate(),
+			day = days[dateObj.getDay()],
+			hour = ('0' + dateObj.getHours()).slice(-2),
+			minute = ('0' + dateObj.getMinutes()).slice(-2),
+			second = ('0' + dateObj.getSeconds()).slice(-2);
+
+		var	dateFormat = day + ' ' + date + ' ' + year, 
+			timeFormat = hour + ':' + minute, // seconds seem unecessary
+		 	millis = dateObj.getTime();
+
+		this.model.set('year', year);
 		this.model.set('date', date);
-		this.model.set('time', time);
-		this.model.set('dateAndTime', dateAndTime);
+		this.model.set('day', day);
+		this.model.set('timeFormat', timeFormat);
+		this.model.set('dateFormat', dateFormat);
+		this.model.set('millis', millis);
+
+		console.log(date);
 
 		// Give it the class of paperId
 		this.$el.addClass(paperId);
